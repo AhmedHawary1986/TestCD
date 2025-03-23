52ca8a4b5f7ec2440e0f7c3611557a42e2b283c3
Ahmed Elhawary <Ahmad.hwry@gmail.com>
2025-03-23 18:02:57 +0200

    Add project files.

---
+++ TestCD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestCD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
